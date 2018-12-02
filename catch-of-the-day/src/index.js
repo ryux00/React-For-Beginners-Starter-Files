b64f5a45087cca001a1f9a8b1b3cb0ffaae5b9f3
Nishit Mohanan <nishitmohanan@gmail.com>
2018-12-03 01:04:22 +0530

    imported css

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 import StorePicker from './components/StorePicker'
-
+import './css/style.css';
 
 render(<StorePicker/>,document.querySelector('#main'));
+
+
+
