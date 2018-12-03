89172f2713e7bf68be41d78c28e7d6650e12c86f
Nishit Mohanan <nishitmohanan@gmail.com>
2018-12-04 00:35:10 +0530

    added header component and app.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,10 +1,11 @@
 // let's go!
 import React from 'react';
 import {render} from 'react-dom';
-import StorePicker from './components/StorePicker'
+import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 
-render(<StorePicker/>,document.querySelector('#main'));
+render(<App/>,document.querySelector('#main'));
 
 
 
