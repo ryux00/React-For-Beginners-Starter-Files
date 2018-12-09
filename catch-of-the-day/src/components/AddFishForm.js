b672173ca8b2fad088be58a85e0863ca122a99c8
Nishit Mohanan <nishitmohanan@gmail.com>
2018-12-09 23:56:18 +0530

    added state

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -1,27 +1,49 @@
 import React from "react";
 
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
 
-class AddFishForm extends React.Component{
-    createFish = (event) => {
-        event.preventDefault();
-        console.log("Making a fish 🐠")
-    }
-    render(){
-        return(
-            <form className="fish-edit" onSubmit={this.createFish}>
-                <input name="name" type="text" placeholder="name"/>
-                <input name="price" type="text" placeholder="price"/>
-                <select name="status">
-                    <option value="available">Fresh !!</option>
-                    <option value="unavailable">Sold Out !!</option>
-                </select>
-                <textarea name="desc" placeholder="desc"></textarea>
-                <input name="image" type="text" placeholder="image"/>
-                <button type="submit">+ Add Fish</button>
-            </form>
-        );
-    }
+  createFish = event => {
+    event.preventDefault();
+    const fish = {
+        name : this.nameRef.value.value,
+        price : parseFloat(this.priceRef.value.value),
+        status : this.statusRef.value.value,
+        desc : this.descRef.value.value,
+        image : this.imageRef.value.value,
+    };
+    this.props.addFish(fish);
+    event.currentTarget.reset()
+  };
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="name" />
+        <input
+          name="price"
+          ref={this.priceRef}
+          type="text"
+          placeholder="price"
+        />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh !!</option>
+          <option value="unavailable">Sold Out !!</option>
+        </select>
+        <textarea name="desc" ref={this.descRef} placeholder="desc" />
+        <input
+          name="image"
+          ref={this.imageRef}
+          type="text"
+          placeholder="image"
+        />
+        <button type="submit">+ Add Fish</button>
+      </form>
+    );
+  }
 }
 
 export default AddFishForm;
-
